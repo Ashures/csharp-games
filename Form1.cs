b1e1c018667d918123386cb992ee95717e6c2dfb
Ashures <ashuresyt@gmail.com>
2023-06-18 14:39:38 +1200

    Created a shell method to deal with KeyDown events and attached it to the form's KeyDown event handler

--- Form1.cs
+++ Form1.cs
@@ -5,5 +5,12 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+
+        this.KeyDown += GetKeyDown;
+    }
+
+    private void GetKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+    {
+        
     }
 }
